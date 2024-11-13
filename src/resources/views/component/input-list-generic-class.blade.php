bf951c44316149e089816d72ca4dcf1836f1cd1d
slavaWins <zxc7613@mail.ru>
2024-11-13 18:48:16 +0700

    list remove tr

--- src/resources/views/component/input-list-generic-class.blade.php
+++ src/resources/views/component/input-list-generic-class.blade.php
@@ -9,6 +9,9 @@
    $attrList =    ReadAttributesConfig::ReadClass(  $element->exampleModelClass);
    $idRand = 'x'.rand(111,9999999);
 
+   function GetOptionsInList($struct) {
+        return $struct['Options'];
+    }
 @endphp
 
 <div class="form-group  blankInputTextGrup blankInputTextGrupSelect mb-2 {{$idRand}}">
@@ -17,54 +20,72 @@
 
 
     <table class="table">
-        <tr>
+        <tr style="font-size: 0.9em;">
+            <td>   <BR>
+                <small> Порядок </small>
+            </td>
             @foreach($attrList as $key=>$struct)
                 <td>
                     {{$struct['Name']}}
                     <BR>
-                    {{$key}}
                     <small> {{$struct['Description']}} </small>
                 </td>
             @endforeach
+            <td>   <BR>
+                <small> Удаление </small>
+            </td>
         </tr>
 
+        <tbody class="_contentList">
         @foreach($value as $valueKey => $item)
-            <tr class="@if(isset($item['__templateData'])) hidden _lineData __templateData @endif   " @if(isset($item['__templateData']))  style="display: none;" @endif>
-                @foreach($attrList as $key=>$struct)
-                    @if(isset($struct['Options']))
-                        <td>
-                            <select type="text" class="form-control noclass is-invalid" id="id_message"
-                                    name="{{(isset($item['__templateData'])  ? "__templateKey__" : "").$element->name .'__'.$key.'['.$valueKey.']'}}">
-                                @foreach($struct['Options'] as $_k=>$_v)
-                                    <option
-                                        @if($item[$key] == $_k) selected @endif
-                                    value="{{$_k}}"> {{$_v}} </option>
-                                @endforeach
-                                <option value="643626">Не валидный int</option>
-                                <option value="asfasfs">Не валидный string</option>
-                            </select>
-                        </td>
-                    @else
-                        <td>
-                            <input type="text" class="form-control noclass is-invalid" id="id_message"
-                                   name="{{(isset($item['__templateData'])  ? "__templateKey__" : "").$element->name .'__'.$key.'['.$valueKey.']'}}"
-                                   value="{{$item[$key]}}"
-                                   inputvalidatorvalues="String"
-                                   @if(isset($struct['Lengh']))
-                                       inputvalidatorvalues-maxlen="{{$struct['Lengh'][1]}}"
-                                   inputvalidatorvalues-minlen="{{$struct['Lengh'][0]}}"
-                                @endif
-                            >
-                        </td>
-                    @endif
+            @if(!empty($item))
+                <tr class="@if(isset($item['__templateData'])) hidden _lineData __templateData @endif   "
+                    @if(isset($item['__templateData']))  style="display: none;" @endif >
 
-                @endforeach
-            </tr>
+                    <td>
+                        <a  style="cursor: pointer" onclick="InputListGenericClassAction_MoveUp(this)"> ⬆️</a>
+                    </td>
+
+                    @foreach($attrList as $key=>$struct)
+                        @if(isset($struct['Options']))
+                            <td>
+                                <select type="text" class="form-control noclass is-invalid" id="id_message"
+                                        name="{{(isset($item['__templateData'])  ? "__templateKey__" : "").$element->name .'__'.$key.'[]'}}">
+                                    @foreach($struct['Options'] as $_k=>$_v)
+                                        <option
+                                            @if($item[$key] == $_k) selected @endif
+                                        value="{{$_k}}"> {{$_v}} </option>
+                                    @endforeach
+                                    <option value="643626">Не валидный int</option>
+                                    <option value="asfasfs">Не валидный string</option>
+                                </select>
+                            </td>
+                        @else
+                            <td>
+                                <input type="text" class="form-control noclass is-invalid" id="id_message"
+                                       name="{{(isset($item['__templateData'])  ? "__templateKey__" : "").$element->name .'__'.$key.'[]'}}"
+                                       value="{{$item[$key]}}"
+                                       inputvalidatorvalues="String"
+                                       @if(isset($struct['Lengh']))
+                                           inputvalidatorvalues-maxlen="{{$struct['Lengh'][1]}}"
+                                       inputvalidatorvalues-minlen="{{$struct['Lengh'][0]}}"
+                                    @endif
+                                >
+                            </td>
+                        @endif
+
+                    @endforeach
+
+                    <td>
+                        <a  style="cursor: pointer" onclick="InputListGenericClassAction_DeleteRow(this)">Удалить</a>
+                    </td>
+
+                </tr>
+            @endif
         @endforeach
+        </tbody>
     </table>
-    <a class="btn  btn-primary " onclick="InputListGenericClassAction_AddRow('.{{$idRand}}')"> + Добавить
-
-    </a>
+    <a class="btn  btn-primary " onclick="InputListGenericClassAction_AddRow('.{{$idRand}}')"> + Добавить  </a>
 
     <small class="form-text text-muted">
         Описание поля
@@ -73,20 +94,33 @@
 </div>
 
 <script>
+    function InputListGenericClassAction_MoveUp(self) {
+        var tr = $(self).closest("tr");
+        var prevTr = tr.prev();
+
+        // Проверяем, существует ли предыдущая строка
+        if (prevTr.length) {
+            // Перемещаем текущий tr перед предыдущим
+            tr.insertBefore(prevTr);
+        }
+    }
+
+    function InputListGenericClassAction_DeleteRow(self) {
+        $(self).closest("tr").remove();
+    }
+
     function InputListGenericClassAction_AddRow(selector) {
         var inp = $(selector);
-        var tbl = inp.find("table");
+        var tbl = inp.find("._contentList");
 
         var tpl = tbl.find(".__templateData");
 
-        var count = tbl.find("._lineData").length - 1;
 
         var e = tpl.clone();
 
         var html = e.html();
-        html = html.replaceAll("[0]", "["+count+"]")
         html = html.replaceAll("__templateKey__", "")
-        e.html( html);
+        e.html(html);
         e.removeClass('__templateData');
         e.removeClass('hidden');
         e.show();
