401f446091f4a2037b2a92529a08f378b142aff8
slavaWins <zxc7613@mail.ru>
2025-03-14 19:26:39 +0700

    approve modal upd

--- src/resources/views/approved-modal.blade.php
+++ src/resources/views/approved-modal.blade.php
@@ -1,24 +1,20 @@
-@php
-    //Топовая штука. Модалка которая спрашивает Точно удалить? перед выполнением какой-то ссылки.
-@endphp
-
 <div class="modal fade" id="modalApproved" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
      aria-hidden="true">
     <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
+        <div class="modal-content" style="padding: 35px;">
 
-                <h5 class=" " id="modalApprovedTitle">??</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+
+            <h5 class=" " id="modalApprovedTitle">??</h5>
+<BR>
+            <div class="row m-0" style="    gap: 0.5em;">
+                <div class="col p-0">
+                    <a class="btn btn-primary btn-gray col-12"   type="button" data-dismiss="modal" onclick=' $("#modalApproved").modal("hide");'>Отменить</a>
+                </div>
+                <div class="col  p-0">
+                    <a  type="submit" href="" class="btn btn-primary   float-end     col-12 mb-2 modalApprovedHrefElement" >Да</a>
+                </div>
             </div>
 
-
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
-                <a type="submit" href="" class="btn btn-danger modalApprovedHrefElement">Да</a>
-            </div>
         </div>
     </div>
 </div>
